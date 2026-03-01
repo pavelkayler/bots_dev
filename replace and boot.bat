a3a793d53bfbbd552616c509aa28c0c00e2b035c
kayler <pavelkayler@icloud.com>
2026-03-01 22:20:15 +0300

    rwe

--- replace and boot.bat
+++ replace and boot.bat
@@ -1,10 +1,27 @@
 @echo off
 setlocal EnableExtensions
-
 cd /d "%~dp0"
 
-call "replace.bat"
-call "m.bat" install
-call "m.bat" start
+echo [1/4] Stop...
+call "m.bat" stop
+if errorlevel 1 goto :err
 
+echo [2/4] Replace (git reset)...
+call "replace.bat"
+if errorlevel 1 goto :err
+
+echo [3/4] Install deps...
+call "m.bat" install
+if errorlevel 1 goto :err
+
+echo [4/4] Start...
+call "m.bat" start
+if errorlevel 1 goto :err
+
+echo Done.
+exit /b 0
+
+:err
+echo ERROR: failed with code %errorlevel%
 pause
+exit /b %errorlevel%
